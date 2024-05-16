68e39aa79ab1779733de0d63cbfebda29501889b
Tshepo Mamabolo Mahudu <tmmmmadi@gmail.com>
2024-05-16 14:41:02 +0200

    initialize

---
+++ src/main/java/mamabologtub/springframework/springtricks/springtricks/SpringtricksApplication.java
@@ -0,0 +1,13 @@
+package mamabologtub.springframework.springtricks.springtricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtricksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mamabologtub/springframework/springtricks/springtricks/SpringtricksApplicationTests.java
@@ -0,0 +1,13 @@
+package mamabologtub.springframework.springtricks.springtricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
