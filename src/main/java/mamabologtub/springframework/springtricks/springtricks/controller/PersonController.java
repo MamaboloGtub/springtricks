129a7baf40d6ec5b042f3d95da9211ed5fb5fd7b
Tshepo Mamabolo Mahudu <tmmmmadi@gmail.com>
2024-05-16 15:16:50 +0200

    controller and service

--- src/main/java/mamabologtub/springframework/springtricks/springtricks/controller/PersonController.java
+++ src/main/java/mamabologtub/springframework/springtricks/springtricks/controller/PersonController.java
@@ -1,9 +1,24 @@
 package mamabologtub.springframework.springtricks.springtricks.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import mamabologtub.springframework.springtricks.springtricks.service.PersonService;
+
 /**
  * Tshepo M Mahudu
  */
 
+@RestController
 public class PersonController {
+    @Autowired
+    private PersonService service;
+
+    @RequestMapping(value = "/person", method = RequestMethod.GET)
+    public String getPerString() {
+        return service.getPerson();
+    }
 
 }
